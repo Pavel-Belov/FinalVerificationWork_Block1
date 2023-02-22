b6a280c8c1c75d631a44d8ee1e38600793d432c1
Pavel Belov <job.pb@yandex.ru>
2023-02-22 20:21:06 +0300

    add program code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввесли либо
+// с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется
+// пользоваться коллекциями, лучше обойтись исключительно массивамию
+
+Console.WriteLine("Введите текст, для разделения используйте пробелы:");
+string text = Console.ReadLine();
+
+var data = text.Split(" ").ToArray();
+string newData = String.Empty;
+
+for (int i = 0; i < data.Length; i++)
+{    
+    if (data[i].Length <= 3) newData += $"{data[i]} ";
+}
+
+newData = newData.Remove(newData.Length - 1);
+var result = newData.Split(" ").ToArray();
+
+Console.WriteLine("Получаем из имеющегося массива массив строк, длина которых меньше либо равна 3 символа:");
+Console.Write("[" + String.Join(", ", data) + "]");
+Console.Write(" -> ");
+Console.WriteLine("[" + String.Join(", ", result) + "]");
