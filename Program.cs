a130d9a26b72cdd0090dcdef842b658df8a281cc
Pavel Belov <job.pb@yandex.ru>
2023-02-22 20:38:09 +0300

    fix: code refactoring, add methods

--- Program.cs
+++ Program.cs
@@ -3,21 +3,34 @@
 // с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется
 // пользоваться коллекциями, лучше обойтись исключительно массивамию
 
-Console.WriteLine("Введите текст, для разделения используйте пробелы:");
-string text = Console.ReadLine();
-
-var data = text.Split(" ").ToArray();
-string newData = String.Empty;
-
-for (int i = 0; i < data.Length; i++)
-{    
-    if (data[i].Length <= 3) newData += $"{data[i]} ";
+string[] GetArrayFromString(string text)
+{
+    return text.Split(" ").ToArray();
 }
 
-newData = newData.Remove(newData.Length - 1);
-var result = newData.Split(" ").ToArray();
+string GetStringFromArray(string[] array)
+{
+    string newData = String.Empty;
 
-Console.WriteLine("Получаем из имеющегося массива массив строк, длина которых меньше либо равна 3 символа:");
+    for (int i = 0; i < array.Length; i++)
+    {    
+        if (array[i].Length <= 3) newData += $"{array[i]} ";
+    }
+
+    if (newData.Length != 0)
+        return newData.Remove(newData.Length - 1);
+    else
+        return newData;
+}
+
+Console.WriteLine("Введите текст, для разделения используйте пробелы:");
+string text = Console.ReadLine();
+var data = GetArrayFromString(text);
+string newText = GetStringFromArray(data);
+var result = GetArrayFromString(newText);
+
+Console.Write("Получаем из имеющегося массива массив строк,");
+Console.WriteLine(" длина которых меньше либо равна 3 символа:");
 Console.Write("[" + String.Join(", ", data) + "]");
 Console.Write(" -> ");
 Console.WriteLine("[" + String.Join(", ", result) + "]");
